50021fed6bfc8afdaf330ebc5e4bd5a7b1d8b337
Xavier Zambrano <xavierzamco@gmail.com>
2023-07-04 14:52:03 -0600

    fix util import

--- TikTokUploader/uploader.py
+++ TikTokUploader/uploader.py
@@ -3,7 +3,7 @@ import json
 import time
 import datetime
 
-from util import assertSuccess, printError, getTagsExtra, uploadToTikTok, log, getCreationId
+from .util import assertSuccess, printError, getTagsExtra, uploadToTikTok, log, getCreationId
 
 
 UA = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
